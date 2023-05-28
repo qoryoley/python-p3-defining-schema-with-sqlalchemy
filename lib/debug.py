767fe598d09d40abbc7227588c7d584e1bb77737
Abdalla <qoryoley@gmail.com>
2023-05-28 14:48:32 +0300

    defining schema with sqlchemy

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
+# lib/sqlalchemy_sandbox.py
+
+#!/usr/bin/env python3
+
+# imports
 from sqlalchemy import create_engine
 
-from sqlalchemy_sandbox import Student
-
-engine = create_engine('sqlite:///students.db')
+# data models
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    engine = create_engine('sqlite:///students.db')
+    Base.metadata.create_all(engine)
